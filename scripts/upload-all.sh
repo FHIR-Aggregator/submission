3b3fc832656738a969166b4aefa2d0901c469f99
Brian Walsh <brian@bwalsh.com>
2025-02-12 12:44:07 -0800

    rename SearchParameter uri

--- scripts/upload-all.sh
+++ scripts/upload-all.sh
@@ -4,4 +4,5 @@ scripts/upload.sh OUTPUT/R4/TCGA-LUAD/META/ TCGA-LUAD
 scripts/upload.sh OUTPUT/R4/TCGA-HNSC/META/ TCGA-HNSC
 scripts/upload.sh OUTPUT/R4/TCGA-BRCA/META/ TCGA-BRCA
 scripts/upload.sh OUTPUT/R4/TCGA-LUSC/META/ TCGA-LUSC
+scripts/upload.sh OUTPUT/R4/IG/META/ IG
 
