4a01cae87fd7f17b73d1a075ba7ce72eec2f926c
Brian Walsh <brian@bwalsh.com>
2025-02-24 18:13:23 -0800

    actions
    skip if no fixture

--- tests/fixtures/README.md
+++ tests/fixtures/README.md
@@ -1,5 +1,1 @@
-# Download test from bucket
-
-```
 aws --profile ceph s3 cp --recursive   --endpoint-url https://rgw.ohsu.edu/ s3://EllrottLab/GDC-FHIR/TCGA-KIRC/ TCGA-KIRC/
-```
--- tests/unit/test_prep.py
+++ tests/unit/test_prep.py
@@ -1,3 +1,5 @@
+import pathlib
+
 import pytest
 
 from click.testing import CliRunner
@@ -10,8 +12,11 @@ def tcga_path():
 
 
 def test_tcga(tcga_path, tmp_path):
-    runner = CliRunner()
-    args = f"prep {tcga_path} {tmp_path} --transformers part-of,vocabulary,validate --fhir-version R4".split()
-    print(args)
-    result = runner.invoke(prep.cli, args)
-    assert result.exit_code == 0, result.output
+    if not pathlib.Path(tcga_path).exists():
+        pytest.skip("TCGA data not found")
+    else:
+        runner = CliRunner()
+        args = f"prep {tcga_path} {tmp_path} --transformers part-of,vocabulary,validate --fhir-version R4".split()
+        print(args)
+        result = runner.invoke(prep.cli, args)
+        assert result.exit_code == 0, result.output
