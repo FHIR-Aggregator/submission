6de264cd33a96d8cf78ef40505bae73ed55cd9c9
Brian Walsh <brian@bwalsh.com>
2025-03-11 10:21:43 -0700

    TCGA projects now in GGC

--- scripts/gs-import-all.sh
+++ scripts/gs-import-all.sh
@@ -3,11 +3,5 @@ scripts/gs-bulk-import-request-IG.sh
 scripts/gs-bulk-import-request-FHIRIZED-CELLOSAURUS.sh
 scripts/gs-bulk-import-request-FHIRIZED-GDC.sh
 scripts/gs-bulk-import-request-FHIRIZED-HTAN.sh
-scripts/gs-bulk-import-request-TCGA-BRCA.sh
-scripts/gs-bulk-import-request-TCGA-ESCA.sh
-scripts/gs-bulk-import-request-TCGA-HNSC.sh
-scripts/gs-bulk-import-request-TCGA-KIRC.sh
-scripts/gs-bulk-import-request-TCGA-LUAD.sh
-scripts/gs-bulk-import-request-TCGA-LUSC.sh
 scripts/gs-configure-search.sh
 
