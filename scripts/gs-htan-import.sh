4d457f749d709fc9765e5cc39c8c08db6478a10c
teslajoy <nasim@plenary.org>
2025-03-11 06:43:54 -0700

    remove comments

--- scripts/gs-htan-import.sh
+++ scripts/gs-htan-import.sh
@@ -7,13 +7,10 @@ set -euo pipefail
 : "${GOOGLE_DATASET:?Need to set GOOGLE_DATASET}"
 : "${GOOGLE_DATASTORE:?Need to set GOOGLE_DATASTORE}"
 
-# Step 1: Preprocess the HTAN input data with fa_submit
 fa_submit prep HTAN/All_HTAN/INPUT_HTAN OUTPUT/R4/HTAN --transformers assay,r4,vocabulary,validate
 
-# Step 2: Upload the transformed data to the appropriate destination
 scripts/upload.sh OUTPUT/R4/HTAN FHIRIZED-HTAN
 
-# Step 3: Trigger the FHIR store import from the GCS bucket
 curl -X POST \
     -H "Authorization: Bearer $(gcloud auth application-default print-access-token)" \
     -H "Content-Type: application/json; charset=utf-8" \
